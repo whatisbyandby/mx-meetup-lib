8e7271d4510717f306ff61b2579d051aa96d1340
Scott Perkins <whatisbyandby@gmail.com>
2024-11-03 21:20:35 -0700

    Added IP Address parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+use core::str::FromStr;
 use cyw43::Control;
 use embassy_net::Stack;
 use embassy_net_driver_channel::Device as D;
@@ -7,10 +8,10 @@ use embassy_rp::adc::{Adc, Async, Channel};
 use embassy_rp::peripherals::USB;
 use embassy_rp::usb::Driver;
 use embassy_usb::class::cdc_acm::Sender;
-use heapless::String;
+use heapless::{String, Vec};
 use serde::{Deserialize, Serialize};
 
-use embassy_time::Timer;
+use embassy_time::{Duration, Instant, Timer};
 
 use embassy_net::tcp::TcpSocket;
 use embassy_net::Ipv4Address;
@@ -22,10 +23,10 @@ use rust_mqtt::utils::rng_generator::CountingRng;
 
 use defmt::info;
 
+use core::fmt::Write;
 use embassy_sync::blocking_mutex::raw::RawMutex;
 use embassy_sync::channel::Receiver;
 use static_cell::StaticCell;
-use core::fmt::Write;
 
 pub mod temperature_sensor;
 
@@ -58,8 +59,10 @@ pub enum PicoCommand {
     Led(bool),
     SetConfig(Configuration),
     Temperature,
+    Reset,
     PrintState,
 }
+
 
 pub struct DemoDeviceBuilder<'a, M>
 where
@@ -71,6 +74,8 @@ where
     control: Option<Control<'a>>,
     adc: Option<(Adc<'a, Async>, Channel<'a>)>,
     id_string: Option<&'static String<16>>,
+    watchdog: Option<embassy_rp::watchdog::Watchdog>,
+    // flash: Option<embassy_rp::flash::Flash>,
 }
 
 impl<'a, M> DemoDeviceBuilder<'a, M>
@@ -84,7 +89,9 @@ where
             stack: None,
             control: None,
             adc: None,
-            id_string: None
+            id_string: None,
+            watchdog: None,
+            // flash: None,
         }
     }
 
@@ -111,6 +118,11 @@ where
         self
     }
 
+    // pub fn with_flash(mut self, flash: embassy_rp::flash::Flash) -> Self {
+    //     self.flash = Some(flash);
+    //     self
+    // }
+
     pub fn with_control(mut self, control: Control<'a>) -> Self {
         self.control = Some(control);
         self
@@ -121,19 +133,25 @@ where
         self
     }
 
+    pub fn with_watchdog(mut self, watchdog: embassy_rp::watchdog::Watchdog) -> Self {
+        self.watchdog = Some(watchdog);
+        self
+    }
 
     pub fn build(self) -> DemoDevice<'a, M> {
         DemoDevice {
             adc: self.adc.unwrap(),
             config: None,
-            current_temperature: 0.0,
             current_led_state: false,
             control: self.control.unwrap(),
             stack: self.stack.unwrap(),
             command_receiver: self.command_receiver.unwrap(),
             id_string: self.id_string.unwrap(),
             usb_sender: self.usb_sender.unwrap(),
-            mqtt_client: None
+            mqtt_client: None,
+            temp_readings: Vec::new(),
+            last_publish: Instant::now(),
+            watchdog: self.watchdog.unwrap(),
         }
     }
 }
@@ -152,14 +170,16 @@ where
 {
     adc: (Adc<'a, Async>, Channel<'a>),
     config: Option<Configuration>,
-    current_temperature: f32,
     current_led_state: bool,
     control: Control<'a>,
     stack: &'a Stack<D<'static, 1514>>,
     command_receiver: Receiver<'a, M, Result<PicoCommand, &'static str>, 5>,
     usb_sender: Sender<'a, Driver<'a, USB>>,
     id_string: &'static String<16>,
-    mqtt_client:  Option<MqttClient<'a, TcpSocket<'a>, 5, CountingRng>>
+    mqtt_client: Option<MqttClient<'a, TcpSocket<'a>, 5, CountingRng>>,
+    temp_readings: Vec<f32, 20>,
+    last_publish: Instant,
+    watchdog: embassy_rp::watchdog::Watchdog,
 }
 
 impl<'a, M> DemoDevice<'a, M>
@@ -171,23 +191,51 @@ where
             .set_power_management(cyw43::PowerManagementMode::PowerSave)
             .await;
 
-        // loop for 30 iterations
-        for _ in 0..15 {
-            Timer::after_millis(100).await;
-            self.control.gpio_set(0, true).await;
-            Timer::after_millis(100).await;
-            self.control.gpio_set(0, false).await;
+        // Blink the LED to indicate the device is starting up
+        for _ in 0..2 {
+            self.set_led(true).await;
+            Timer::after_millis(1000).await;
+            self.set_led(false).await;
+            Timer::after_millis(1000).await;
         }
 
         // Wait until the device is configured
         while self.config.is_none() {
             self.check_for_command().await;
+            Timer::after_millis(1000).await;
+            self.set_led(true).await;
             Timer::after_millis(100).await;
+            self.set_led(false).await;
         }
 
+        self.watchdog.start(Duration::from_millis(5000));
         self.connect_wifi().await;
+        self.watchdog.feed();
         self.connect_mqtt().await;
+        self.watchdog.feed();
     }
+
+    // pub fn read_config(&self) -> Result<Configuration, &'static str> {
+    //     let config_string = self.command_receiver.as_ref().unwrap().try_receive();
+    //     match config_string {
+    //         Ok(config) => match config {
+    //             Ok(config) => Ok(config),
+    //             Err(_) => Err("Failed to read config"),
+    //         },
+    //         Err(_) => Err("Failed to read config"),
+    //     }
+    // }
+
+    // pub fn write_config(&self, config: Configuration) -> Result<(), &'static str> {
+    //     let config_string = serde_json_core::to_string::<Configuration, 256>(&config)
+    //         .map_err(|_| "Failed to serialize config")?;
+    //     self.usb_sender
+    //         .as_ref()
+    //         .unwrap()
+    //         .write_packet(config_string.as_bytes())
+    //         .unwrap();
+    //     Ok(())
+    // }
 
     pub async fn connect_wifi(&mut self) {
         let ssid = self.config.as_ref().unwrap().wifi_ssid.as_str();
@@ -205,6 +253,7 @@ where
                 }
             }
         }
+        self.watchdog.feed();
 
         // Wait for DHCP, not necessary when using static IP
         self.usb_sender
@@ -215,6 +264,7 @@ where
         while !self.stack.is_config_up() {
             Timer::after_millis(100).await;
         }
+        self.watchdog.feed();
         self.usb_sender
             .write_packet("DHCP is now up!\n".as_bytes())
             .await
@@ -229,10 +279,20 @@ where
         while !self.stack.is_link_up() {
             Timer::after_millis(500).await;
         }
+        self.watchdog.feed();
         info!("Link is up!");
+        self.usb_sender
+            .write_packet("Link is up!\n".as_bytes())
+            .await
+            .unwrap();
 
         info!("waiting for stack to be up...");
+        self.usb_sender
+            .write_packet("waiting for stack to be up...\n".as_bytes())
+            .await
+            .unwrap();
         self.stack.wait_config_up().await;
+        self.watchdog.feed();
         info!("Stack is up!");
         self.usb_sender
             .write_packet("Stack is up!\n".as_bytes())
@@ -243,26 +303,31 @@ where
     pub async fn connect_mqtt(&mut self) {
         static RX_BUFFER: StaticCell<[u8; 4096]> = StaticCell::new();
         static TX_BUFFER: StaticCell<[u8; 4096]> = StaticCell::new();
-    
+
         let rx_buffer: [u8; 4096] = [0; 4096];
         let tx_buffer: [u8; 4096] = [0; 4096];
-    
+
         let static_rx_buffer = RX_BUFFER.init(rx_buffer);
         let static_tx_buffer = TX_BUFFER.init(tx_buffer);
 
         let mut socket = TcpSocket::new(self.stack, static_rx_buffer, static_tx_buffer);
-        
+
         socket.set_timeout(None);
-        let address = Ipv4Address::from_bytes(&[192, 168, 1, 88]);
+
+
+        let address = Ipv4Address::from_str(self.config.as_ref().unwrap().mqtt_server.as_str())
+            .map_err(|_| "Failed to parse IP address")
+            .unwrap();
+        
         let remote_endpoint = (address, 1883);
         socket.connect(remote_endpoint).await.unwrap();
-
+        self.watchdog.feed();
 
         let mut config = ClientConfig::new(
             rust_mqtt::client::client_config::MqttVersion::MQTTv5,
             CountingRng(20000),
         );
-    
+
         config.add_max_subscribe_qos(rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1);
         let mut client_id = String::<64>::new();
         client_id.push_str("device-").unwrap();
@@ -270,27 +335,26 @@ where
         static CLIENT_ID: StaticCell<String<64>> = StaticCell::new();
         let static_client_id = CLIENT_ID.init(client_id);
         config.add_client_id(static_client_id.as_str());
-    
+
         let mut will_topic = String::<64>::new();
         will_topic.push_str("device/").unwrap();
         will_topic.push_str(self.id_string.as_str()).unwrap();
         will_topic.push_str("/status").unwrap();
         static WILL_TOPIC: StaticCell<String<64>> = StaticCell::new();
         let static_will_topic = WILL_TOPIC.init(will_topic);
-    
+
         config.add_will(static_will_topic.as_str(), "DISCONNECTED".as_bytes(), true);
         config.max_packet_size = 100;
-    
+
         let recv_buffer = [0u8; 256];
         static RECV_BUFFER: StaticCell<[u8; 256]> = StaticCell::new();
         let static_recv_buffer = RECV_BUFFER.init(recv_buffer);
-    
+
         let write_buffer = [0u8; 256];
         static WRITE_BUFFER: StaticCell<[u8; 256]> = StaticCell::new();
         let static_write_buffer = WRITE_BUFFER.init(write_buffer);
 
-        let mut client: MqttClient<'a, TcpSocket<'a>, 5, CountingRng> =
-        MqttClient::<_, 5, _>::new(
+        let mut client: MqttClient<'a, TcpSocket<'a>, 5, CountingRng> = MqttClient::<_, 5, _>::new(
             socket,
             static_write_buffer,
             256,
@@ -299,7 +363,6 @@ where
             config,
         );
 
-
         match client.connect_to_broker().await {
             Ok(()) => info!("MQTT Connected"),
             Err(mqtt_error) => match mqtt_error {
@@ -307,14 +370,22 @@ where
                 ReasonCode::Success => info!("Success"),
                 _ => info!("Another Error {:?}", mqtt_error),
             },
-        }      
+        }
+        self.watchdog.feed();
 
-        client.send_message(static_will_topic.as_str(), "CONNECTED".as_bytes(), rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1, true).await.unwrap();
+        client
+            .send_message(
+                static_will_topic.as_str(),
+                "CONNECTED".as_bytes(),
+                rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1,
+                true,
+            )
+            .await
+            .unwrap();
+        self.watchdog.feed();
 
         self.mqtt_client = Some(client);
-
     }
-   
 
     pub async fn check_for_command(&mut self) {
         let command_ready = self.command_receiver.try_receive();
@@ -345,10 +416,24 @@ where
     }
 
     pub async fn run(&mut self) {
-        self.check_for_command().await;
-        let raw_reading = self.adc.0.read(&mut self.adc.1).await.unwrap();
-        let temp_c = convert_to_celsius(raw_reading);
-        self.set_temperature(temp_c);
+        loop {
+            self.watchdog.feed();
+            self.check_for_command().await;
+            self.read_temperature().await;
+
+            if self.last_publish.elapsed() > Duration::from_millis(1000) {
+                self.publish_state().await;
+                self.last_publish = Instant::now();
+            }
+
+            Timer::after_millis(50).await;
+        }
+    }
+
+    pub async fn publish_state(&mut self) {
+        if self.mqtt_client.is_none() {
+            return;
+        }
         let json_state = self.get_state_json().unwrap();
 
         let mut state_topic = String::<64>::new();
@@ -356,7 +441,17 @@ where
         state_topic.push_str(self.id_string.as_str()).unwrap();
         state_topic.push_str("/state").unwrap();
 
-        self.mqtt_client.as_mut().unwrap().send_message(state_topic.as_str(), json_state.as_bytes(), rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1, true).await.unwrap();
+        self.mqtt_client
+            .as_mut()
+            .unwrap()
+            .send_message(
+                state_topic.as_str(),
+                json_state.as_bytes(),
+                rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1,
+                true,
+            )
+            .await
+            .unwrap();
     }
 
     pub fn set_config(&mut self, config: Configuration) {
@@ -367,12 +462,22 @@ where
         self.config.as_ref()
     }
 
-    pub fn set_temperature(&mut self, value: f32) {
-        self.current_temperature = value;
+    pub async fn read_temperature(&mut self) -> f32 {
+        let raw_reading = self.adc.0.read(&mut self.adc.1).await.unwrap();
+        let temp_c = convert_to_celsius(raw_reading);
+        if self.temp_readings.len() == 20 {
+            self.temp_readings.pop().unwrap();
+        }
+        self.temp_readings.push(temp_c).unwrap();
+        temp_c
     }
 
-    pub fn get_temperature(&self) -> f32 {
-        self.current_temperature
+    pub fn get_avg_temperature(&self) -> f32 {
+        let mut sum = 0.0;
+        for temp in self.temp_readings.iter() {
+            sum += temp;
+        }
+        sum / self.temp_readings.len() as f32
     }
 
     pub async fn set_led(&mut self, value: bool) {
@@ -382,7 +487,7 @@ where
 
     pub fn get_state(&self) -> DeviceState {
         DeviceState {
-            temperature: self.current_temperature,
+            temperature: self.get_avg_temperature(),
             led_state: self.current_led_state,
         }
     }
@@ -403,7 +508,16 @@ where
                 message_string
                     .push_str("LED set\n")
                     .map_err(|_| "Failed to set LED\n")?;
+                self.publish_state().await;
                 Ok(message_string)
+            }
+            PicoCommand::Reset => {
+                self.watchdog.trigger_reset();
+                let mut message: String<64> = String::new();
+                message
+                    .push_str("Resetting\n")
+                    .map_err(|_| "Failed to reset\n")?;
+                Ok(message)
             }
             PicoCommand::SetConfig(config) => {
                 self.set_config(config);
@@ -414,12 +528,13 @@ where
                 Ok(message_string)
             }
             PicoCommand::Temperature => {
-                let raw_reading = self.adc.0.read(&mut self.adc.1).await.unwrap();
-                let temp_c = convert_to_celsius(raw_reading);
-                self.set_temperature(temp_c);
+                let temp_c = self.read_temperature().await;
                 let mut message_string = String::new();
-                write!(message_string, "{:.2}", temp_c).map_err(|_| "Failed to read temperature\n")?;
-                message_string.push_str(" °C\n").map_err(|_| "Failed to read temperature\n")?;
+                write!(message_string, "{:.2}", temp_c)
+                    .map_err(|_| "Failed to read temperature\n")?;
+                message_string
+                    .push_str("° C\n")
+                    .map_err(|_| "Failed to read temperature\n")?;
                 Ok(message_string)
             }
             PicoCommand::PrintState => {
@@ -457,6 +572,13 @@ pub fn parse_command(input: String<256>) -> Result<PicoCommand, &'static str> {
                 return Err("TEMP command does not take any parameters\n");
             }
             return Ok(PicoCommand::Temperature);
+        }
+        "RESET" => {
+            let param = iter.next().is_some();
+            if param {
+                return Err("RESET command does not take any parameters\n");
+            }
+            return Ok(PicoCommand::Reset);
         }
         "CONFIG" => {
             let config_str = iter
